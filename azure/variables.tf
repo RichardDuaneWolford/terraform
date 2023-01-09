e81ad2cba12574f6e18d1b68dbaed30e0fb1c9b1
RichardDuaneWolford <38475828+RichardDuaneWolford@users.noreply.github.com>
2023-01-09 16:40:50 -0500

    Create variables.tf

---
+++ azure/variables.tf
@@ -0,0 +1,18 @@
+variable "name" {
+  
+  description = "The resource group name"
+  type = string
+  
+}
+
+variable "location" {
+  
+  description = "The region where the resource group will be made."
+  type = string  
+  default = "east-us"
+  
+  validation {
+    condition = contains(["east-us", "east-us2", "west-us", "central-us"], var.location)
+    error_message = "The location provided is either not valid or not currently supported."
+  }
+  
